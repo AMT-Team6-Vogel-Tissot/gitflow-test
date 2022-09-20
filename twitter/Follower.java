5b1f7e885d2cf2d8377ddf0d1c5d2c0168c138e0
maelle <maelle.vogel@heig-vd.ch>
2022-09-20 16:51:40 +0200

    implement method

--- twitter/Follower.java
+++ twitter/Follower.java
@@ -1,2 +1,6 @@
 public class Follower implements IObserver {
+    @Override
+    public void update(IObservable observable) {
+
+    }
 }
--- twitter/Twitter.java
+++ twitter/Twitter.java
@@ -1,2 +1,18 @@
+import java.util.List;
+
 public class Twitter implements IObservable {
+    @Override
+    public void Subscribe(List<IObserver> observer) {
+
+    }
+
+    @Override
+    public void Unsubscribe(IObserver observer) {
+
+    }
+
+    @Override
+    public void Notify() {
+
+    }
 }
